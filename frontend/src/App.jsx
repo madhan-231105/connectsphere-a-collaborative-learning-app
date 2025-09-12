c35605a7937962f730d272f623b5b6466f424fc2
madhan-231105 <mathangopi23@gmail.com>
2025-09-12 15:42:02 +0530

    Initial commit of project folders

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
